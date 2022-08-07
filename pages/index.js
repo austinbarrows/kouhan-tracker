bf0fb787fe11ee568a54d3699f21405678499052
Austin Barrows <austinm.barrows@gmail.com>
2022-08-07 00:26:03 -0400

    Remove Home css file b/c Tailwind makes it useless

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Sidebar from "../components/sidebar";
 
 export default function Home() {
