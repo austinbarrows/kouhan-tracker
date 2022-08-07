f1887bc8449457479e627353f03baf3892fb2283
Austin Barrows <austinm.barrows@gmail.com>
2022-08-07 16:49:08 -0400

    Remove useless space

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta
           name="description"
           content="Kouhan Tracker tracks a wide variety of things in a flexible manner to act as a versatile all-in-one tracking and calender application. Kouhan Tracker can be used to track habits, daily tasks such as video game daily quests, skills in development, events--recurring and singular--and really almost anything else one would want to track."
-        />{" "}
+        />
         {/* Potentially update/improve this description later; this is more of a quick placeholder */}
       </Head>
       <Sidebar>
