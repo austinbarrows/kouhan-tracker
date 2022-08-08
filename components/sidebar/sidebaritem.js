9621b58ebe23045c09c376d09ae1f5cfd5ba40fb
Austin Barrows <austinm.barrows@gmail.com>
2022-08-08 00:03:36 -0400

    Make cursor pointer on link sidebar item hover

--- components/sidebar/sidebaritem.js
+++ components/sidebar/sidebaritem.js
@@ -6,6 +6,7 @@ export default function SidebarItem({ children, footer, href }) {
   }`;
 
   if (href) {
+    className += " cursor-pointer";
     return (
       <Link href={href}>
         <div className={className}>{children}</div>
