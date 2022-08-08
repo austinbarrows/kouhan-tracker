960e827fa72815c507d56a19fe570aad70a6fa08
Austin Barrows <austinm.barrows@gmail.com>
2022-08-08 03:30:12 -0400

    Add Mantine capability to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: "light" }}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
