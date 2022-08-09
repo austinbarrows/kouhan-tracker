513c903ddf557d1b54893dee4221837795571df7
Austin Barrows <austinm.barrows@gmail.com>
2022-08-09 03:47:05 -0400

    Remove old sidebar and main components

--- components/sidebar/sidebar.js
+++
@@ -1,7 +0,0 @@
-export default function Sidebar({ children }) {
-  return (
-    <div className="relative top-0 left-0 h-screen w-56 bg-green-300 float-left p-0 break-words flex-none">
-      {children}
-    </div>
-  );
-}
--- components/sidebar/sidebarfooter.js
+++
@@ -1,9 +0,0 @@
-import SidebarItem from "components/sidebar/sidebaritem";
-
-export default function SidebarFooter({ children }) {
-  return (
-    <div className="absolute bottom-0 w-full">
-      <SidebarItem footer>{children}</SidebarItem>
-    </div>
-  );
-}
