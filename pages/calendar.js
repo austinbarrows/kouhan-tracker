8dfbec88eac6c0f39074ce44022202bc02bb3b42
Austin Barrows <austinm.barrows@gmail.com>
2022-08-10 05:21:21 -0400

    Add grid test/experimenting code

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,8 +1,46 @@
-import { Container } from "@mantine/core";
+import { Container, Grid, Skeleton } from "@mantine/core";
 import CoreLayout from "components/corelayout";
 
 export default function Calendar({}) {
-  return <Container className="px-0">Test calendar text new</Container>;
+  return (
+    <div>
+      <Grid>
+        <Grid.Col span={12}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={8}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={7}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <Skeleton className="h-12 rounded-lg" animate={false}></Skeleton>
+        </Grid.Col>
+      </Grid>
+    </div>
+  );
 }
 
 Calendar.getLayout = function getLayout(page) {
