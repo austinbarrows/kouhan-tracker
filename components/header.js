928fb4bc4baf564d87c6d66be7818ca872635b5f
Austin Barrows <austinm.barrows@gmail.com>
2022-08-10 16:56:55 -0400

    Make entire app name div link; add cursor pointer

--- components/header.js
+++ components/header.js
@@ -7,9 +7,11 @@ export default function AppHeader({}) {
     <Header height={55}>
       <Container fluid className="flex justify-between h-full items-center">
         {/* App name/icon div set to 11.9375 rem width because I felt like it was 1 pixel too big to align with the navbar items when set to 12rem */}
-        <div className="p-2 font text-2xl bg-gray-100 rounded-lg w-[11.9375rem] text-center">
-          <Link href="/">広範 Tracker</Link>
-        </div>
+        <Link href="/">
+          <div className="p-2 font text-2xl bg-gray-100 rounded-lg w-[11.9375rem] text-center cursor-pointer">
+            広範 Tracker
+          </div>
+        </Link>
         <DarkModeToggle></DarkModeToggle>
       </Container>
     </Header>
