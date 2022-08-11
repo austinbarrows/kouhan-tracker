21feba8b67ca5db70fb0189d94f1ed62e3928966
Austin Barrows <austinm.barrows@gmail.com>
2022-08-11 06:02:35 -0400

    Fix dark mode on site logo

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Header, Group, Container } from "@mantine/core";
+import { Header, Group, Container, Box } from "@mantine/core";
 import { DarkModeToggle } from "./darkmodetoggle";
 
 export default function AppHeader({}) {
@@ -8,9 +8,25 @@ export default function AppHeader({}) {
       <Container fluid className="flex justify-between h-full items-center">
         {/* App name/icon div set to 11.9375 rem width because I felt like it was 1 pixel too big to align with the navbar items when set to 12rem */}
         <Link href="/">
-          <div className="p-2 font text-2xl bg-gray-100 rounded-lg w-[11.9375rem] text-center cursor-pointer">
+          <Box
+            className="p-2 text-2xl w-[11.9375rem] text-center cursor-pointer"
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              borderRadius: theme.radius.md,
+
+              "&:hover": {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[5]
+                    : theme.colors.gray[1],
+              },
+            })}
+          >
             広範 Tracker
-          </div>
+          </Box>
         </Link>
         <DarkModeToggle></DarkModeToggle>
       </Container>
