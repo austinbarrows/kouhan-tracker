f394fb4b5e47d0a5d12e0f0af27efb55f22b12fe
Austin Barrows <austinm.barrows@gmail.com>
2022-08-11 20:32:38 -0400

    Add more spacing options to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        100: "25rem",
+        104: "26rem",
+        108: "27rem",
+        112: "28rem",
+        116: "29rem",
+        120: "30rem",
+        124: "31rem",
         128: "32rem",
       },
     },
