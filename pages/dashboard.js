90e7152328b17753d58fd391b5c203de84ce1fe4
Austin Barrows <austinm.barrows@gmail.com>
2022-08-17 02:58:12 -0400

    Add preliminary real getCalendarData code

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -31,11 +31,15 @@ export const getServerSideProps = withAuthUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
 })(async ({ AuthUser }) => {
   // Optionally, get other props.
+  const startDate = "2022-8-20";
+  // User ID token uniquely identifies a user with their firebase uid and verifies they are legitimately logged in (or their login info has been stolen lol)
   const token = await AuthUser.getIdToken();
   const response = await fetch("http://localhost:3000/api/getCalendarData", {
-    method: "GET",
-    headers: {
-      Authorization: token,
+    method: "POST",
+    body: {
+      authorization: token,
+      startDate: startDate,
+      numberOfDays: 7,
     },
   });
   const data = await response.json();
