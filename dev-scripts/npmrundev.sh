d327c7e312b94b2613186f2b6b296af04a78e397
Austin Barrows <austinm.barrows@gmail.com>
2022-09-30 03:30:43 -0400

    Change npmrundev to auto-restart on first CTRL+C

--- dev-scripts/npmrundev.sh
+++ dev-scripts/npmrundev.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 cd ../
 npm run dev
+
+# Script basis source: https://stackoverflow.com/a/28722706
+trap_ctrlC() {
+    exit 1
+}
+
+trap trap_ctrlC SIGINT SIGTERM
+
+while true; do
+    npm run dev
+done
