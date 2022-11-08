de2ca7ae8b1adb32f46fb6595ee7e1fa0b3386c9
Austin Barrows <austinm.barrows@gmail.com>
2022-11-07 23:24:46 -0500

    Update register API route user addition code

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,11 +3,11 @@ import clientPromise from "lib/mongodb";
 import { getFirebaseAdmin } from "next-firebase-auth";
 import initAuth from "initAuth";
 import mongoose from "mongoose";
-const { Schema } = mongoose;
+const { Schema, model } = mongoose;
 
 initAuth();
 
-const blogSchema = new Schema({
+const userSchema = new Schema({
   userID: String, // String is shorthand for {type: String}
   displayName: String,
   calendar: {
@@ -15,6 +15,8 @@ const blogSchema = new Schema({
     dates: [],
   },
 });
+
+const User = model("User", userSchema);
 
 const handler = async (req, res) => {
   if (req.method !== "POST") {
@@ -24,25 +26,40 @@ const handler = async (req, res) => {
   // Validate user ID token and throw error if invalid
   const idToken = req.headers.authorization;
   const fbAdmin = getFirebaseAdmin();
-  let userID;
+  let newUser;
   try {
-    userID = (await fbAdmin.auth().verifyIdToken(idToken)).uid;
+    newUser = await fbAdmin.auth().verifyIdToken(idToken);
   } catch (e) {
     console.log(e);
     res.status(400).json({ error: "Could not verify user" });
     return true;
   }
 
+  const userID = newUser.uid;
+  const displayName = newUser.displayName;
+
   // MongoDB setup
   const client = await clientPromise;
   const db = client.db("kt-test");
   const collection = db.collection("users");
 
+  await mongoose.connect(process.env.MONGODB_URI + "/" + "kt-test");
+  const dbMongoose = mongoose.connection;
+
+  // Test to see if user already exists
   const userSearchResult = await collection.find({ userID: userID }).toArray();
   if (userSearchResult.length !== 0) {
     res.status(400).json({ error: "User is already registered" });
     return true;
   }
+
+  // Add new user to database
+  const newUserDoc = new User({
+    userID: userID,
+    displayName: displayName,
+    calendar: { recurring: {}, dates: [] },
+  });
+  const addResult = await newUserDoc.save();
 
   return res.status(200).json({ success: true });
 };
