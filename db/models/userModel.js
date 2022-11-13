731290a716b277375241e61197210d713d8672de
Austin Barrows <austinm.barrows@gmail.com>
2022-11-13 02:20:07 -0500

    Rename `recurring` to `events` due to schema shift

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   userID: String, // String is shorthand for {type: String}
   displayName: String,
   calendar: {
-    recurring: { type: Map, of: { type: Object } }, // Maybe a pointless way to define this? But also possibly safer (due to POSSIBLE prototype pollution prevention)???
+    events: { type: Map, of: { type: Object } }, // Maybe a pointless way to define this? But also possibly safer (due to POSSIBLE prototype pollution prevention)???
     dates: [],
   },
 });
