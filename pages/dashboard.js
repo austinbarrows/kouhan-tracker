15b2fb60c0a29e6631f04215cb3c1d9d27b6603f
Austin Barrows <austinm.barrows@gmail.com>
2023-03-06 18:30:16 -0500

    Remove old test code from Dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -19,7 +19,6 @@ const Dashboard = (props) => {
           ? `Logged-in user's name: ${AuthUser.displayName}`
           : "Not logged in"}
       </Box>
-      <Box>{`Logged-in user's firebase uid: ${props.userID}`}</Box>
     </Box>
   );
 };
@@ -30,44 +29,6 @@ Dashboard.getLayout = function getLayout(page) {
 
 export const getServerSideProps = withAuthUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
-})(async ({ AuthUser }) => {
-  // Optionally, get other props.
-  const startDate = dayjs().format();
-  // User ID token uniquely identifies a user with their firebase uid and verifies they are legitimately logged in (or their login info has been stolen lol)
-  const token = await AuthUser.getIdToken();
-  console.log("before response");
-  // DO NOT USE API PATH in production; instead, directly import database access logic
-  const response = await fetch("http://localhost:3000/api/getCalendarData", {
-    method: "POST",
-    headers: {
-      authorization: token,
-    },
-    body: JSON.stringify({
-      startDate: startDate,
-      numberOfDays: 7,
-    }),
-  });
-
-  try {
-    const data = await response.json();
-    console.log(data);
-    if (data.userID !== undefined) {
-      return {
-        props: {
-          userID: data.userID,
-        },
-      };
-    } else {
-      throw new Error("No userID in response JSON");
-    }
-  } catch (e) {
-    console.log(e);
-    return {
-      props: {
-        userID: null,
-      },
-    };
-  }
-});
+})();
 
 export default withAuthUser()(Dashboard);
